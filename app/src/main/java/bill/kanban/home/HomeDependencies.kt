23c76297b0ddf4145711574d705bbc9a93f4ec8c
Emanuel Machado da Silva <emanuel.mch@gmail.com>
2017-09-02 22:00:20 -0300

    HomeDependencies cleaning up

--- app/src/main/java/bill/kanban/home/HomeDependencies.kt
+++ app/src/main/java/bill/kanban/home/HomeDependencies.kt
@@ -3,27 +3,23 @@ package bill.kanban.home
 import dagger.Component
 import dagger.Module
 import dagger.Provides
-import javax.inject.Singleton
 
-@Module
-class HomeModule(private val fragment: HomeFragment) {
-    @Provides
-    fun provideHomeView(): HomeView = fragment
+object HomeDependencies {
+    fun setup(fragment: HomeFragment) {
+        DaggerHomeComponent.builder()
+                .homeModule(HomeModule(fragment))
+                .build()
+                .inject(fragment)
+    }
 }
 
-@Singleton
 @Component(modules = arrayOf(HomeModule::class))
 interface HomeComponent {
     fun inject(fragment: HomeFragment)
 }
 
-object HomeDependencies {
-    fun setup(fragment: HomeFragment) {
-        val component = DaggerHomeComponent.builder()
-                .homeModule(HomeModule(fragment))
-                .build()
-
-        component.inject(fragment)
-
-    }
+@Module
+class HomeModule(private val fragment: HomeFragment) {
+    @Provides
+    fun provideHomeView(): HomeView = fragment
 }
